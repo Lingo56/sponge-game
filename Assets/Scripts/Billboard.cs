ada511021b3cc1bfc23df828ba7f1efc57edd89e
Peter Soava <thelingo56@gmail.com>
2025-07-27 07:26:08 -0700

    Add bus, boat, and rockstar models

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    [SerializeField] private float scaleFactor = 3f; // Scale factor for the billboard
+    private Transform cam;
+
+    void Start()
+    {
+        Texture texture = GetComponent<Renderer>().material.mainTexture;
+        float aspectRatio = (float)texture.width / texture.height;
+        
+        transform.localScale = new Vector3(aspectRatio * scaleFactor, scaleFactor, scaleFactor);
+        
+        cam = Camera.main.transform;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 camPosition = cam.position;
+        camPosition.y = transform.position.y; // Ignore vertical angle
+        transform.LookAt(camPosition);
+    
+        // Rotate 180 degrees around the up axis
+        transform.Rotate(0f, 180f, 0f);
+    }
+
+}
